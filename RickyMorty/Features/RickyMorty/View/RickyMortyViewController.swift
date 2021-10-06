b1b22c44264f45fe7ed9b3261d2dea926943519e
Melikhan Hoşdoğdu <melikhan.hosdogdu@gmail.com>
2021-10-06 22:04:26 +0300

    Initial Commit

---
+++ RickyMorty/Features/RickyMorty/View/RickyMortyViewController.swift
@@ -0,0 +1,18 @@
+//
+//  RickyMortyViewController.swift
+//  RickyMorty
+//
+//  Created by Melikhan Hosdogdu on 6.10.2021.
+//
+
+import UIKit
+
+final class RickyMortyViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
